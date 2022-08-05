fd1194a52ab43fe055f360aeb5e6d9ba5bf71a7b
LatikaJadhav94 <latikaj189@gmail.com>
2022-08-05 23:42:22 +0530

    SMS 01 : Repository and Service Impl created

---
+++ src/main/java/com/bikkadIt/service/StudentServiceImpl.java
@@ -0,0 +1,33 @@
+package com.bikkadIt.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bikkadIt.model.Student;
+import com.bikkadIt.repository.StudentRepository;
+
+public class StudentServiceImpl implements StudentServiceI {
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public int addStudent(Student student) {
+		Student save=studentRepository.save(student);
+		return save.getStudentId();
+	}
+
+	@Override
+	public List<Student> getAllStudent() {
+		List<Student> findAll = studentRepository.findAll();
+		return findAll;
+	}
+
+	@Override
+	public Student getStudentById(Integer id) {
+		Student findById= studentRepository.findById(id).get();
+		return findById;
+	}
+
+}
