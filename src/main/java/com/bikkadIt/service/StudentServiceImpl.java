db5a5e9ac046baf0935bb1e6da374b12cf15593a
LatikaJadhav94 <latikaj189@gmail.com>
2022-08-05 13:00:28 +0530

    SMS 02: ServiceI interface created

---
+++ src/main/java/com/bikkadIt/service/StudentServiceImpl.java
@@ -0,0 +1,32 @@
+//package com.bikkadIt.service;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import com.bikkadIt.model.Student;
+//
+//public class StudentServiceImpl implements StudentServiceI {
+//
+//	@Autowired
+//	private StudentRepository studentRepository;
+//	
+//	@Override
+//	public int addStudent(Student student) {
+//		studentRepository.sav
+//		return 0;
+//	}
+//
+//	@Override
+//	public List<Student> getAllStudent() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public Student getStudentId(Integer id) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//}
